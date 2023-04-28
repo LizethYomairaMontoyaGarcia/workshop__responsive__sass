b69593597e107dcd92991e9cd1a267296dcd250e
lizeth montoya <lizethmontoyagarcia@gmail.com>
2023-04-27 22:20:36 -0500

    crando funcionalidad de feature

--- src/app/button.js
+++ src/app/button.js
@@ -1,20 +1,22 @@
 
   
   /* Botones desplegables*/
+
   function myFunction() {
-      console.log("esta es la funcion");
     document.getElementById("myDropdown").classList.toggle("show");
     
   }
   // Cierra el menú desplegable si el usuario hace clic fuera de él.
   window.onclick = function (e) {
+    console.log("evento del boton ")
     if (!e.target.matches(".header__dropbtn")) {
+        console.log("dentro del if")
       let dropdowns = document.getElementsByClassName("header__dropdown-content");
       let i;
       for (i = 0; i < dropdowns.length; i++) {
         const openDropdown = dropdowns[i];
         if (openDropdown.classList.contains("show")) {
-          openDropdown.classList.remove("show");
+          //openDropdown.classList.remove("show");
         }
       }
     }

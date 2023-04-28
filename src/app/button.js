aa973ba3fcc5b36b1000a8fb61e0520caa004c77
lizeth montoya <lizethmontoyagarcia@gmail.com>
2023-04-27 23:14:51 -0500

    lista las funcionalidades de los botones

--- src/app/button.js
+++ src/app/button.js
@@ -1,24 +1,29 @@
+/* Botones desplegables*/
 
-  
-  /* Botones desplegables*/
+function myFunction(idButton) {
+  document.getElementById(idButton).classList.toggle("show");
+}
+// Cierra el menú desplegable si el usuario hace clic fuera de él.
+window.onclick = function (e) {
+  console.log("evento del boton ");
+  //matches indica que coincida las class
+  if (!e.target.matches(".header__dropbtn__feature")) {
+    closeDropdown("header__dropdown__feature");
 
-  function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-    
+  } else if (!e.target.matches(".header__dropbtn__company")) {
+    closeDropdown("header__dropdown__company");
   }
-  // Cierra el menú desplegable si el usuario hace clic fuera de él.
-  window.onclick = function (e) {
-    console.log("evento del boton ")
-    if (!e.target.matches(".header__dropbtn")) {
-        console.log("dentro del if")
-      let dropdowns = document.getElementsByClassName("header__dropdown-content");
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
-        const openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains("show")) {
-          //openDropdown.classList.remove("show");
-        }
-      }
+};
+
+function closeDropdown(nameDropdown) {
+  let dropdowns = document.getElementsByClassName(nameDropdown);
+  console.log("open del dropw", dropdowns);
+  let i;
+  for (i = 0; i < dropdowns.length; i++) {
+    const openDropdown = dropdowns[i];
+    console.log("open del dropw", openDropdown);
+    if (openDropdown.classList.contains("show")) {
+      openDropdown.classList.remove("show");
     }
-  };
-  
+  }
+}
